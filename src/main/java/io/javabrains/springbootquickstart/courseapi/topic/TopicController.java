b145587ef1e6fd6fe044f7c76413ee5b0dadb333
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-16 02:37:44 +0530

    spring dependency injection types

--- src/main/java/io/javabrains/springbootquickstart/courseapi/topic/TopicController.java
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/topic/TopicController.java
@@ -8,14 +8,58 @@ import java.util.List;
 @RestController
 public class TopicController {
 
-    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
-    @Autowired
-    private TopicService topicService;
+    // Spring Dependency Injection Types:
+    //
+    // 01. Constructor-based dependency injection - Class constructor is annotated with @Autowired and includes a
+    // variable number of arguments with the objects to be injected
+    //
+    // 02. Setter-based dependency injection - Setter methods are annotated with @Autowired and Spring container will
+    // call these setter methods once the Bean is instantiated
+    //
+    // 03. Field-based dependency injection - Fields/properties are annotated with @Autowired and Spring container will
+    // set these fields once the class is instantiated
 
+    // final keyword in Java:
+    // If a variable is final, its value cannot be changed (constant)
+    // If a method is final, it cannot be overridden
+    // If a class is final, it cannot be extended
     private final TopicService topicServiceConstructor;
 
-    public TopicController(TopicService topicService) {
-        this.topicServiceConstructor = topicService;
+    // Field-based dependency injection is elegant, avoids the boilerplate code for getters and setters or creating
+    // constructors for the class
+    // But it has the following drawbacks:
+    // 01. Disallows final/immutable field declaration
+    // 02. Eases single responsibility principle violation
+    // 03. Tightly coupled with dependency injection container
+    // 04. Hidden dependencies
+
+    // Field-based dependency injection
+    @SuppressWarnings("SpringJavaAutowiredFieldsWarningInspection")
+    @Autowired // Annotation is mandatory
+    private TopicService topicService;
+
+    // Field injection is not recommended, because the list of required dependencies are unclear during instance
+    // creation which makes testing more difficult and could lead to runtime exceptions when the bean is instantiated
+    // without spring
+    //
+    // To clearly state the required dependencies of the class, they should be added to the constructor that has the
+    // @Autowired annotation, so that these dependencies can be provided more easily through Spring's dependency
+    // injection mechanism
+    //
+    // Constructor-based injection is recommended for required dependencies allowing them to be immutable and
+    // preventing them to be null
+    //
+    // Setter-based injection is recommended for optional dependencies
+    //
+    // As of Spring Framework 4.3, an @Autowired annotation on such a constructor is no longer necessary if the target
+    // bean only defines one constructor to begin with
+    // However, if several constructors are available, at least one must be annotated to teach the container which one
+    // to use
+
+    // Constructor-based dependency injection
+    @Autowired // Annotation is optional
+    public TopicController(TopicService topicServiceConstructor) {
+        this.topicServiceConstructor = topicServiceConstructor;
     }
 
     @PostMapping("/topics")
