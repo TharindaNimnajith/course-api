378b6d568cbac0ac97957605178b3f6f6a3f6a0b
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-16 23:30:30 +0530

    maven

---
+++ target/spring-aot/test/sources/org/springframework/test/context/aot/AotTestAttributes__Generated.java
@@ -0,0 +1,16 @@
+package org.springframework.test.context.aot;
+
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Generated map for {@link AotTestAttributes}.
+ */
+public class AotTestAttributes__Generated {
+  public static Map<String, String> getAttributes() {
+    Map<String, String> map = new HashMap<>();
+    map.put("org.springframework.boot.test.context.SpringBootTestContextBootstrapper.SpringBootConfiguration.io.javabrains.springbootquickstart.courseapi.CourseApiApplicationTests", "io.javabrains.springbootquickstart.courseapi.CourseApiApplication");
+    return map;
+  }
+}
---
+++ target/spring-aot/test/sources/org/springframework/test/context/aot/AotTestContextInitializers__Generated.java
@@ -0,0 +1,26 @@
+package org.springframework.test.context.aot;
+
+import java.lang.Class;
+import java.lang.String;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Generated mappings for {@link AotTestContextInitializers}.
+ */
+public class AotTestContextInitializers__Generated {
+  public static Map<String, Supplier<ApplicationContextInitializer<? extends ConfigurableApplicationContext>>> getContextInitializers(
+      ) {
+    Map<String, Supplier<ApplicationContextInitializer<? extends ConfigurableApplicationContext>>> map = new HashMap<>();
+    return map;
+  }
+
+  public static Map<String, Class<? extends ApplicationContextInitializer<?>>> getContextInitializerClasses(
+      ) {
+    Map<String, Class<? extends ApplicationContextInitializer<?>>> map = new HashMap<>();
+    return map;
+  }
+}
