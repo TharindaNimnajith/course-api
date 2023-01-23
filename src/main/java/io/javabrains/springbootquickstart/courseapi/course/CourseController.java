53c910f7785dd0e2d8eea8cf32b8c2e78cb778b2
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-23 17:44:25 +0530

    course api get methods

--- src/main/java/io/javabrains/springbootquickstart/courseapi/course/CourseController.java
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/course/CourseController.java
@@ -1,5 +1,6 @@
 package io.javabrains.springbootquickstart.courseapi.course;
 
+import io.javabrains.springbootquickstart.courseapi.newtopic.NewTopic;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,29 +15,51 @@ public class CourseController {
         this.courseService = courseService;
     }
 
-    @PostMapping("/courses")
-    public void addCourse(@RequestBody Course course) {
+    @PostMapping("topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setNewTopic(new NewTopic(topicId, "", ""));
         courseService.saveCourse(course);
     }
 
-    @PutMapping("/courses")
-    public void updateCourse(@RequestBody Course course) {
+    @PutMapping("topics/{topicId}/courses")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setNewTopic(new NewTopic(topicId, "", ""));
         courseService.saveCourse(course);
     }
 
-    @DeleteMapping("/courses/{id}")
+    @SuppressWarnings("MVCPathVariableInspection")
+    @DeleteMapping("topics/{topicId}/courses/{id}")
     public void deleteCourse(@PathVariable String id) {
         courseService.deleteCourse(id);
     }
 
-    @GetMapping("/courses/{id}")
+    @SuppressWarnings("MVCPathVariableInspection")
+    @GetMapping("topics/{topicId}/courses/{id}")
     public Course getCourse(@PathVariable String id) {
         return courseService.getCourse(id);
     }
 
-    @GetMapping("/courses")
+    @SuppressWarnings("MVCPathVariableInspection")
+    @GetMapping("topics/{topicId}/courses/all")
     public List<Course> getCourses() {
         return courseService.getCourses();
+    }
+
+    @SuppressWarnings("MVCPathVariableInspection")
+    @GetMapping("topics/{topicId}/courses/name/{name}")
+    public List<Course> getCoursesByName(@PathVariable String name) {
+        return courseService.getCoursesByName(name);
+    }
+
+    @SuppressWarnings("MVCPathVariableInspection")
+    @GetMapping("topics/{topicId}/courses/description/{description}")
+    public List<Course> getCoursesByDescription(@PathVariable String description) {
+        return courseService.getCoursesByDescription(description);
+    }
+
+    @GetMapping("topics/{topicId}/courses")
+    public List<Course> getCoursesByTopicId(@PathVariable String topicId) {
+        return courseService.getCoursesByTopicId(topicId);
     }
 
 }
