cc25ecfe727e5bc5d69108a7e1f84f4133314885
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2023-01-29 12:55:24 +0530

    jpa repository inherited methods

--- src/main/java/io/javabrains/springbootquickstart/courseapi/lesson/LessonRepository.java
+++ src/main/java/io/javabrains/springbootquickstart/courseapi/lesson/LessonRepository.java
@@ -13,6 +13,8 @@ import java.util.List;
 //
 // CrudRepository
 //
+// Interface for generic CRUD operations on a repository for a specific type
+//
 // 01. <S extends T> S save(S entity)
 //     - saves a given entity
 //     - use the returned instance for further operations as the save operation might have changed the entity instance
@@ -73,38 +75,64 @@ import java.util.List;
 //
 // ListCrudRepository
 //
+// Interface for generic CRUD operations on a repository for a specific type
+// This an extension to CrudRepository returning List instead of Iterable where applicable
+//
 // 01. <S extends T> List<S> saveAll(Iterable<S> entities)
-//     -
+//     - saves all given entities
+//     - params: entities (must not be null nor must it contain null)
+//     - returns: the saved entities (will never be null and will be the same size as the Iterable passed as params)
 //
 // 02. List<T> findAll()
-//     -
+//     - returns all instances of the type
+//     - returns: all entities
 //
 // 03. List<T> findAllById(Iterable<ID> ids)
-//     -
+//     - returns all instances of the type T with the given ids
+//     - if some or all ids are not found, no entities are returned for these ids
+//     - note that the order of elements in the result is not guaranteed
+//     - params: ids (must not be null nor contain any null values)
+//     - returns: entities with the given ids (guaranteed to be not null, the size <= the number of given ids)
 //
 //
 // PagingAndSortingRepository
 //
+// Repository fragment to provide methods to retrieve entities using the pagination and sorting abstraction
+// This is combined with CrudRepository or similar or with manually added methods to provide CRUD functionality
+//
 // 01. Iterable<T> findAll(Sort sort)
-//     -
+//     - returns all entities sorted by the given options
+//     - params: sort (the Sort specification to sort the results by, can be Sort.unsorted(), must not be null)
+//     - returns: all entities sorted by the given options
 //
 // 01. Page<T> findAll(Pageable pageable)
-//     -
+//     - returns a Page of entities meeting the paging restriction provided in the Pageable object
+//     - params: pageable (the pageable to request a paged result, can be Pageable.unpaged(), must not be null)
+//     - returns: a page of entities
 //
 //
 // ListPagingAndSortingRepository
 //
+// Repository fragment to provide methods to retrieve entities using the pagination and sorting abstraction
+// This an extension to PagingAndSortingRepository returning List instead of Iterable where applicable
+//
 // 01. List<T> findAll(Sort sort)
-//     -
+//     - returns all entities sorted by the given options
+//     - params: sort (the Sort specification to sort the results by, can be Sort.unsorted(), must not be null)
+//     - returns: all entities sorted by the given options
 //
 //
 // QueryByExampleExecutor
+//
+// Interface to allow execution of Query by Example (QBE) Example instances
 //
 // 01.
 //     -
 //
 //
 // JpaRepository
+//
+// JPA specific extension of org.springframework.data.repository.Repository
 //
 // 01.
 //     -
